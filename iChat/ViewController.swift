86ace0c0f54a54d47220e7484ed5bb89f9fa1de7
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2019-07-23 21:06:47 +0700

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Christian Doxa Hamasiah on 23/07/19.
+//  Copyright © 2019 Christian Doxa Hamasiah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
